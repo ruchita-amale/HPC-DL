497bfe78407bd36b297a2b9033e7c0bf93022296
Ruchita Amale <68845346+ruchita-amale@users.noreply.github.com>
2024-04-27 04:21:33 +0530

    prac

---
+++ HPC/assign3.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <omp.h>
+
+using namespace std;
+using namespace chrono;
+
+// Sequential implementation of Min, Max, Sum, and Average
+template <typename T>
+void sequentialOperations(const vector<T>& data, T& minVal, T& maxVal, T& sum, double& average) {
+    minVal = data[0];
+    maxVal = data[0];
+    sum = data[0];
+    for (size_t i = 1; i < data.size(); ++i) {
+        if (data[i] < minVal) minVal = data[i];
+        if (data[i] > maxVal) maxVal = data[i];
+        sum += data[i];
+    }
+    average = static_cast<double>(sum) / data.size();
+}
+
+// Parallel implementation of Min, Max, Sum, and Average using OpenMP reduction
+template <typename T>
+void parallelOperations(const vector<T>& data, T& minVal, T& maxVal, T& sum, double& average) {
+    minVal = data[0];
+    maxVal = data[0];
+    sum = data[0];
+    #pragma omp parallel for reduction(min:minVal) reduction(max:maxVal) reduction(+:sum)
+    for (size_t i = 1; i < data.size(); ++i) {
+        if (data[i] < minVal) minVal = data[i];
+        if (data[i] > maxVal) maxVal = data[i];
+        sum += data[i];
+    }
+    average = static_cast<double>(sum) / data.size();
+}
+
+int main() {
+    // Generate some random data
+    const size_t size = 10000000;
+    vector<int> data(size);
+    for (size_t i = 0; i < size; ++i) {
+        data[i] = rand() % 1000;
+    }
+
+    // Sequential operations
+    int minValSeq, maxValSeq, sumSeq;
+    double averageSeq;
+    auto startSeq = high_resolution_clock::now();
+    sequentialOperations(data, minValSeq, maxValSeq, sumSeq, averageSeq);
+    auto stopSeq = high_resolution_clock::now();
+    auto durationSeq = duration_cast<milliseconds>(stopSeq - startSeq);
+
+    // Parallel operations
+    int minValPar, maxValPar, sumPar;
+    double averagePar;
+    auto startPar = high_resolution_clock::now();
+    parallelOperations(data, minValPar, maxValPar, sumPar, averagePar);
+    auto stopPar = high_resolution_clock::now();
+    auto durationPar = duration_cast<milliseconds>(stopPar - startPar);
+
+    // Output results
+    cout << "Sequential Min: " << minValSeq << endl;
+    cout << "Sequential Max: " << maxValSeq << endl;
+    cout << "Sequential Sum: " << sumSeq << endl;
+    cout << "Sequential Average: " << averageSeq << endl;
+    cout << "Sequential Execution Time: " << durationSeq.count() << " milliseconds" << endl;
+
+    cout << "\nParallel Min: " << minValPar << endl;
+    cout << "Parallel Max: " << maxValPar << endl;
+    cout << "Parallel Sum: " << sumPar << endl;
+    cout << "Parallel Average: " << averagePar << endl;
+    cout << "Parallel Execution Time: " << durationPar.count() << " milliseconds" << endl;
+
+    return 0;
+}
